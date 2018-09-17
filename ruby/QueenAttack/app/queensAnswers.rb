595f6a17177b6682befc6a451c806ee0afdff2cc
paulapaulTW <ppaul@thoughtworks.com>
2018-09-17 09:44:56 -0400

    added answers file for ruby QueensAttack and fixed syntax in tests

---
+++ ruby/QueenAttack/app/queensAnswers.rb
@@ -0,0 +1,31 @@
+class QueensAnswers
+
+  def initialize(white_position: [0,0], black_position: [0,0])
+    @white = white_position
+    @black = black_position
+  end
+
+  def attack?
+    if valid_position? then
+      return (same_rank? || same_file? || diagonal?)
+    else
+      raise ArgumentError.new("Argument Error")
+    end
+  end
+
+  def same_rank? #'rank' is the row on the board - chess positions are stated row-column / [rank,file]
+    @black[0] == @white[0]
+  end
+
+  def same_file? 
+    @black[1] == @white[1]
+  end
+
+  def diagonal?
+    (@black[0] - @white[0]).abs == (@black[1] - @white[1]).abs
+  end
+
+  def valid_position?
+    return ((@black[0] >= 0) && (@black[1] >= 0) && (@black[0] < 8) && (@black[1] < 8) && (@white[0] >= 0) && (@white[1] >= 0) && (@white[0] < 8) && (@white[1] < 8))
+  end
+end
--- ruby/QueenAttack/spec/queens_spec.rb
+++ ruby/QueenAttack/spec/queens_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../app/queens'
+require_relative '../app/queensAnswers' #for testing answers file
 
 describe Queens do
   describe "#attack?" do
@@ -7,56 +8,107 @@ describe Queens do
       expect(queens.attack?).to eq(false)
     end
 
-    xit "can attack on same rank" do
+    it "can attack on same rank" do
       queens = Queens.new(black_position: [2, 4], white_position: [2, 6])
       expect(queens.attack?).to eq(true)
     end
 
-    xit "can attack on same file" do
+    it "can attack on same file" do
       queens = Queens.new(black_position: [4, 5], white_position: [2, 5])
       expect(queens.attack?).to eq(true)
     end
 
-    xit "can attack on first diagonal" do
+    it "can attack on first diagonal" do
       queens = Queens.new(black_position: [2, 2], white_position: [0, 4])
       expect(queens.attack?).to eq(true)
     end
 
-    xit "can attack on second diagonal" do
+    it "can attack on second diagonal" do
       queens = Queens.new(black_position: [2, 2], white_position: [3, 1])
       expect(queens.attack?).to eq(true)
     end
 
-    xit "can attack on third diagonal" do
+    it "can attack on third diagonal" do
       queens = Queens.new(black_position: [2, 2], white_position: [1, 1])
       expect(queens.attack?).to eq(true)
     end
 
-    xit "can attack on fourth diagonal" do
+    it "can attack on fourth diagonal" do
       queens = Queens.new(black_position: [2, 2], white_position: [5, 5])
       expect(queens.attack?).to eq(true)
     end
 
-    xit "raises an error if the rank is negative" do
-      expect{ Queens.new(black_position: [-2, 2]) }.
-        to raise_error(ArgumentError)
+    it "raises an error if the rank is negative" do
+      expect { queens = Queens.new(black_position: [-2, 2], white_position: [1, 1]).attack? }.to raise_error(ArgumentError)
     end
 
-    xit "raises an error if the rank is off the board" do
-      expect{ Queens.new(black_position: [8, 4]) }.
-        to raise_error(ArgumentError)
+    it "raises an error if the rank is off the board" do
+      expect { Queens.new(black_position: [8, 4], white_position: [5, 5]).attack? }.to raise_error(ArgumentError)
     end
 
-    xit "raises an error if the file is negative" do
-    expect{ Queens.new(black_position: [2, -2]) }.
-      to raise_error(ArgumentError)
+    it "raises an error if the file is negative" do
+      expect { Queens.new(black_position: [2, -2], white_position: [5, 5]).attack? }.to raise_error(ArgumentError)
     end
 
-    xit "raises an error if the file is off the board" do
-      expect{ Queens.new(black_position: [4, 8]) }.
-        to raise_error(ArgumentError)
+    it "raises an error if the file is off the board" do
+      expect { Queens.new(black_position: [4, 8], white_position: [5, 5]).attack? }.to raise_error(ArgumentError)
     end
   end
+
+# Tests for queensAnswers.rb file - don't peek!
+# describe QueensAnswers do
+#   describe "#attack?" do
+#     it "cannot attack on different rank and file" do
+#       queens = QueensAnswers.new(black_position: [2, 4], white_position: [6, 6])
+#       expect(queens.attack?).to eq(false)
+#     end
+
+#     it "can attack on same rank" do
+#       queens = QueensAnswers.new(black_position: [2, 4], white_position: [2, 6])
+#       expect(queens.attack?).to eq(true)
+#     end
+
+#     it "can attack on same file" do
+#       queens = QueensAnswers.new(black_position: [4, 5], white_position: [2, 5])
+#       expect(queens.attack?).to eq(true)
+#     end
+
+#     it "can attack on first diagonal" do
+#       queens = QueensAnswers.new(black_position: [2, 2], white_position: [0, 4])
+#       expect(queens.attack?).to eq(true)
+#     end
+
+#     it "can attack on second diagonal" do
+#       queens = QueensAnswers.new(black_position: [2, 2], white_position: [3, 1])
+#       expect(queens.attack?).to eq(true)
+#     end
+
+#     it "can attack on third diagonal" do
+#       queens = QueensAnswers.new(black_position: [2, 2], white_position: [1, 1])
+#       expect(queens.attack?).to eq(true)
+#     end
+
+#     it "can attack on fourth diagonal" do
+#       queens = QueensAnswers.new(black_position: [2, 2], white_position: [5, 5])
+#       expect(queens.attack?).to eq(true)
+#     end
+
+#     it "raises an error if the rank is negative" do
+#       expect { queens = QueensAnswers.new(black_position: [-2, 2], white_position: [1, 1]).attack? }.to raise_error(ArgumentError)
+#     end
+
+#     it "raises an error if the rank is off the board" do
+#       expect { QueensAnswers.new(black_position: [8, 4], white_position: [5, 5]).attack? }.to raise_error(ArgumentError)
+#     end
+
+#     it "raises an error if the file is negative" do
+#       expect { QueensAnswers.new(black_position: [2, -2], white_position: [5, 5]).attack? }.to raise_error(ArgumentError)
+#     end
+
+#     it "raises an error if the file is off the board" do
+#       expect { QueensAnswers.new(black_position: [4, 8], white_position: [5, 5]).attack? }.to raise_error(ArgumentError)
+#     end
+#   end
 
   #Refactoring tip: Extract the queen position into a class
 end
