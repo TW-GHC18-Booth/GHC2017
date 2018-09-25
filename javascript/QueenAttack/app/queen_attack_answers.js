8768a85ef23fd3c8ca8e2468b69a936f789baa37
Ylia M <ysmorid@users.noreply.github.com>
2018-09-24 22:15:34 -0700

    Add clarifying comments

--- javascript/QueenAttack/app/queen_attack_answers.js
+++ javascript/QueenAttack/app/queen_attack_answers.js
@@ -8,14 +8,17 @@ const queen_attack = (black_position, white_position) => {
   return isRankValid() || isFileValid() || isDiagonalValid();
 };
 
+// Checks if rank (row) is the same
 const isRankValid = () => {
   return blackPosition[0] === whitePosition[0]
 };
 
+// Checks if file (column) is the same
 const isFileValid = () => {
   return blackPosition[1] === whitePosition[1];
 };
 
+// Checks if position of black and white are diagonal to each other
 const isDiagonalValid = () => {
   return Math.abs(blackPosition[0] - whitePosition[0]) === Math.abs(blackPosition[1] - whitePosition[1]);
 };
